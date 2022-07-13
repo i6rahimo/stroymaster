fde3c1761f30eec41c77489df8cd70e1db3f1b2f
i6rahim <i6rahim210@gmail.com>
2022-07-13 22:17:29 +0300

    "create slider"

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -3,10 +3,34 @@ import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-    
     pagination: {
       el: '.swiper-pagination',
     },  
-});
+  });
   
-  
+const teamslider = new Swiper ('.team__slide', {
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+  direction: 'horizontal',
+  loop: true,
+  slideClass: 'team__slider',
+  wrapperClass: 'team__slider-wrapper',
+  pagination: {
+    el: '.team__swiper-pagination',
+    type: 'fraction',
+  },
+  pagination: {
+    el: '.team__swiper-pagination2',
+    type: 'bullets',
+  },
+  navigation: {
+    nextEl: '.team-button-prev',
+    prevEl: '.team-button-next',
+  },
+  breakpoints: {
+    
+  }
+})
+
+
+console.log(teamslider);
